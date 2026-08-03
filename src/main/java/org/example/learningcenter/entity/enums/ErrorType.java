adcf42609c3ae5af478a11e7e6de00d93237384c
muhammadali <boronking03@gmail.com>
2026-08-03 16:03:02 +0500

    added enrollemnt crud APIs

--- src/main/java/org/example/learningcenter/entity/enums/ErrorType.java
+++ src/main/java/org/example/learningcenter/entity/enums/ErrorType.java
@@ -42,8 +42,7 @@ public enum ErrorType {
     INVALID_FILE_TYPE("invalid.file.type", HttpStatus.BAD_REQUEST),
     INVOICE_NOT_FOUND("invoice.not.found", HttpStatus.NOT_FOUND),
     STUDENT_NOT_FOUND("student.not.found", HttpStatus.NOT_FOUND),
-    INVOICE_COUNTER_NOT_FOUND("invoice.counter.not.found", HttpStatus.NOT_FOUND),
-    ENROLLMENT_NOT_FOUND("enrollment.not.found", HttpStatus.NOT_FOUND);
+    INVOICE_COUNTER_NOT_FOUND("invoice.counter.not.found", HttpStatus.NOT_FOUND);
 
 
     private final String msg;
