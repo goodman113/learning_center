476284f2fedf8b8ceadece0516b91781ea039454
developer <nurilloh54@gmail.com>
2026-08-12 16:40:44 +0500

    Branch.java

--- src/main/java/org/example/learningcenter/service/BranchService.java
+++ src/main/java/org/example/learningcenter/service/BranchService.java
@@ -22,7 +22,7 @@ public class BranchService extends AbstractService<
 
     @Override
     public Page<BranchDto> getAll(Pageable pageable, String search) {
-
+        return null;
     }
 
     @Override
